6d8b55931e91066b38c6f6aa0f2b10ab2c177b0c
Simonas Rupšys <Simonas.Rupsys@adform.com>
2016-05-16 16:06:16 +0300

    fix path (directories created in /home/root instead of /root)

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,14 +25,14 @@ class nvm (
   }
 
   if $nvm_dir == undef {
-    $final_nvm_dir = "/home/${user}/.nvm"
+    $final_nvm_dir = "${home}/.nvm"
   }
   else {
     $final_nvm_dir = $nvm_dir
   }
 
   if $profile_path == undef {
-    $final_profile_path = "/home/${user}/.bashrc"
+    $final_profile_path = "${home}/.bashrc"
   }
   else {
     $final_profile_path = $profile_path
